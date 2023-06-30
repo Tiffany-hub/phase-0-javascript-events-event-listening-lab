9a3185162baaf82e563657ec24b720a37f4c71c3
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-06-30 10:04:22 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const button = document.getElementById("button")
+
+function clickAlert(){
+        alert('I was clicked');
+    }
+
 function addingEventListener() {
+    button.addEventListener('click', clickAlert);
+
 }
+    // const input = document.getElementById('input');
+    // const button = document.getElementById('button');
+
+   
+    // input.addEventListener('click', clickAlert);
+    
+addingEventListener();
